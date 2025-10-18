3c76fbfdff2edb2e95d97b3ea06cf876c089d865
AradD7 <arad.delaramnikoo@torontomu.ca>
2025-10-17 23:00:34 -0400

    added about project paragraph first draft

--- pages/about/AboutProject.jsx
+++ pages/about/AboutProject.jsx
@@ -1,5 +1,25 @@
 export default function AboutProject() {
     return (
-        <h1>In About Project</h1>
+        <p>Every dev knows the feeling of coming up with an idea in the middle of night for a project
+            and getting all excited and start planning every detail for hours before falling sleep and waking
+            up the next day motivated to build something amazing, only to fall into an ancient trap.
+            This trap, is of course, the infamous "why stop here?" trap, aka "why think so small" strap, aka
+            "why not make millions off of this" trap, you get the point. Why stop with a simple budgeting app,
+            why not build a financial tracker and investment planning app that connects to my tv and thermostat
+            while automatically tracks my tasks and reminds me through the custom emailing protocol that I will
+            definitely make by mysleft completely from from scratch? The desire to turn a simple idea into the
+            biggest, most profitable app ever is a great enemy to a lot of developers. Which brings us to this project!
+            I initially wanted to put my Go knowledge to use and make a simple Knucklebones game with online play because
+            there ain't a good one out there! Naturally, at first I thought I'll make the backend and just have a cli app
+            to interact and play. But I wanted my friends and family to be able to play also! I can't get my mom to use the
+            command line to roll a dice and place it in a 2x2 matrix! So naturally I had to make a frontend for it, which is
+            why I spent weeks to learn React and made this website. But wait, why stop at one simple dice game where I can
+            make an entire roster of games built with mostly the same backend? Which finally brings us to Silly Mini Games!
+            Silly Mini Game is a gamehub with lesser known, fun and engaging 2 player games that all use the same backend
+            for connecting, authenticating and account management. At this moment only Knucklebones is available but I
+            will be adding other games in the future (wink wink).
+            But no fr I will actually add other games. For now, enjoy Knucklebones! Message me with an invite link even!
+            Lastly checkout my github page and thank you for taking the time and intrest in my project.
+        </p>
     )
 }
